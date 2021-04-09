abb287f1081d92ad5180b8563061b600fe3791ed
lnh <lnhdyx@outlook.com>
2021-04-09 21:03:59 +0800

    init project

---
+++ producer/src/test/java/producer/ProducerApplicationTest.java
@@ -0,0 +1,13 @@
+package producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
