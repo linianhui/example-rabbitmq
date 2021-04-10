dffdc8205098990756d5f2533c3230ad410f458c
lnh <lnhdyx@outlook.com>
2021-04-10 20:09:28 +0800

    add consumer2

--- consumer/src/main/java/consumer/LogAdapter.java
+++ consumer/src/main/java/consumer/LogAdapter.java
@@ -1,7 +1,6 @@
 package consumer;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ListOperations;
@@ -18,8 +17,9 @@ public class LogAdapter {
 
     public void log(String format, Object... args) {
         final String message = String.format(format, args);
+        final String hostName = System.getenv().get("HOSTNAME");
         log.info(message);
-        list.rightPush(LOG_KEY, OffsetDateTime.now().toString() + " " + message);
+        list.rightPush(LOG_KEY, hostName + " " + OffsetDateTime.now().toString() + " " + message);
     }
 
 }
